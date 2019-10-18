297a0e0f64f1c79644c3a7a344ce625c79b6671f
John Hlavin <jhlavin@crcfe01.crc.nd.edu>
2019-10-18 11:23:09 -0400

    added full table code, updated outputs

--- methanogens.sh
+++ methanogens.sh
@@ -1,9 +1,11 @@
 #creates list of proteomes that have the McrA gene and the number of copies of the HSP70 gene in that proteome
 #usage: bash methanogens.sh 
+#setup: put muscle, hmmbuild, hmmsearch in "executables" directory within your working directory
 
-#STEP 1: clear working directory of conflicting files and compile all reference sequences into one fasta file
+#STEP 1: clear working directory of old conflicting files and compile all reference sequences into one fasta file
 
-rm hsp70* mcrA* final* output*
+rm hsp70* mcrA* candidate* output* full* table*
+
 cat ref_sequences/mcrA* >> mcrA.ref
 cat ref_sequences/hsp70* >> hsp70.ref
 
@@ -38,8 +40,21 @@ do
 	egrep "^$i:" hsp70_counts.txt | cut -d : -f 1,3 | sed -e 's/ \+/ /g' | sed 's/\[number of targets reported over threshold\]//g' >> output.txt
 done
 
-#STEP 5: add header to output file
+#STEP 5: add header to output file and sort highest to lowest by number of pH resistance genes
 
-echo "Methanogen proteome number: number of pH resistance genes" > finalOutput.txt
-cat output.txt >> finalOutput.txt
-cat finalOutput.txt
+echo "Methanogen proteome number: number of pH resistance genes" > candidateList.txt
+cat output.txt | sort -r -n -k2 >> candidateList.txt
+
+#STEP 6: create full table of all results
+
+grep "Domain search space" mcrA_searchOut.txt | grep -n "Domain search space" | cut -d : -f 1,3 | sed -e 's/ \+/ /g' | sed 's/\[number of targets reported over threshold\]//g' > tableM.txt
+grep "Domain search space" hsp70_searchOut.txt | grep -n "Domain search space" | cut -d : -f 1,3 | sed -e 's/ \+/ /g' | sed 's/\[number of targets reported over threshold\]//g' > tableH.txt
+
+echo "Proteome ID, # mcrA, # hsp70" > fullTable.txt
+
+for i in {1..50}
+do
+	var=$(egrep "^$i:" tableM.txt)
+	var2=$(egrep "^$i:" tableH.txt | cut -d : -f 2)
+	echo $var $var2 >> fullTable.txt
+done
